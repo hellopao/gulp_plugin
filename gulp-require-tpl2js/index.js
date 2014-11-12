5c6d5e73086936b799638cb7224a360c4ccc4503
hellopao <qinjia@outlook.com>
2014-11-12 11:46:00 +0800

    fixed

--- gulp-require-tpl2js/index.js
+++ gulp-require-tpl2js/index.js
@@ -24,21 +24,24 @@ module.exports = function(options) {
         var defaultExtNames = [".html",".tpl"];
         var fileExtname = path.extname(file.path);
 
-        if (!options.extname && defaultExtNames.indexOf(fileExtname) === -1) {
-            this.emit('error', new gutil.PluginError(PLUGIN_NAME, fileExtname + ' file not supported'));
-            return cb();
+        if (defaultExtNames.indexOf(fileExtname) > -1 || (options.extname && options.extname.replace(/^\.?/,'.') === fileExtname)) {
+            try {
+                var tpl = file.contents.toString();
+                var lines = tpl.replace(/^\s*$/gm,'').split(/\r?\n/);
+                
+                for (var i = 0; i < lines.length; i++) {
+                    lines[i] = lines[i].replace(/'/g,'\\\'').replace(/^(\s*)/g,'\t$1\'').replace(/(\s)*$/,'\'$1');
+                }
+
+                tpl = ';define(function(){\r\n\t return ' +  '[\r\n\t' + lines.join(",\r\n\t") + '\r\n\t].join("");\r\n});';
+            } catch (err) {
+                
+            }
+
+            file.contents = new Buffer(tpl);
         }
         
-        var tpl = file.contents.toString();
-        var lines = tpl.replace(/^\s*$/gm,'').split(/\r?\n/);
-        
-        for (var i = 0; i < lines.length; i++) {
-            lines[i] = lines[i].replace(/'/g,'\\\'').replace(/^(\s*)/g,'\t$1\'').replace(/(\s)*$/,'\'$1');
-        }
 
-        tpl = ';define(function(){\r\n\t return ' +  '[\r\n\t' + lines.join(",\r\n\t") + '\r\n\t].join("");\r\n});';
-
-        file.contents = new Buffer(tpl);
         this.push(file);
 		cb();
 	});
