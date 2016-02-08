2e7a1d950d665cadb45ea9605a18568944643c33
Aaron Bernstein <aaron@stitchmedia.ca>
2016-02-08 16:02:43 -0500

    a better fix

--- gulp-make-css-url-version/index.js
+++ gulp-make-css-url-version/index.js
@@ -67,12 +67,12 @@ module.exports = function (options) {
 
         var promises = [];
 
-        html = html.replace(/url\("?([^\)"]+)"?\)/g, function (str, url) {
+        html = html.replace(/url\(["|']?([^\)#"']+)["|']?\)/g, function (str, url) {
 
             url = url.replace(/\?[\s\S]*$/, "").trim();
             url = url.replace(/['"]*/g, "");
 
-            if (url.indexOf("base64,") > -1 || url.indexOf("about:blank") > -1 || url.indexOf("http://") > -1 || url === '/') {
+            if (url.indexOf("base64,") > -1 || url.indexOf("about:blank") > -1 || url.indexOf("http://") > -1 || url === '/' || url === '') {
                 return str;
             }
 
