83604f4b7359554918b2213d9a30b332946778b0
Aaron Bernstein <aaron@stitchmedia.ca>
2016-02-08 15:59:20 -0500

    Revert "fix for url (#foo) and url("")"
    This reverts commit 2f6a76339910a70a612b2a2395c81fe12789d9a6.

--- gulp-make-css-url-version/index.js
+++ gulp-make-css-url-version/index.js
@@ -68,10 +68,11 @@ module.exports = function (options) {
         var promises = [];
 
         html = html.replace(/url\("?([^\)"]+)"?\)/g, function (str, url) {
-            
-            url = url.replace(/['"]*/g, "").replace(/#[\s\S]*$/,'');
 
-            if (url.indexOf("base64,") > -1 || url.indexOf("about:blank") > -1 || url.indexOf("http://") > -1 || url === '/' || url === '') {
+            url = url.replace(/\?[\s\S]*$/, "").trim();
+            url = url.replace(/['"]*/g, "");
+
+            if (url.indexOf("base64,") > -1 || url.indexOf("about:blank") > -1 || url.indexOf("http://") > -1 || url === '/') {
                 return str;
             }
 
@@ -83,11 +84,12 @@ module.exports = function (options) {
             }
 
             //use md5
+            var safeUrl = url.replace(/#[\s\S]*$/,'');
             var imageFilePath;
             if (options.assetsDir) {
-                imageFilePath = path.join(options.assetsDir, url);
+                imageFilePath = path.join(options.assetsDir, safeUrl);
             } else {
-                imageFilePath = path.resolve(path.dirname(file.path), url);
+                imageFilePath = path.resolve(path.dirname(file.path), safeUrl);
             }
 
             var tempKey = Math.random().toString();
